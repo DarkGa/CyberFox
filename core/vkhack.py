3ede544084ca09ae38a4ba7384614863e5574006
DarkGa <48998251+DarkGa@users.noreply.github.com>
2019-05-18 18:44:58 +0300

    Update vkhack.py

--- core/vkhack.py
+++ core/vkhack.py
@@ -1,17 +1,4 @@
 from core.colors import *
 import time
-import os
-import sys
-token=input(B+"token your group: "+W)
-f = open('/data/data/com.termux/files/home/CyberFox/core/hackVK.py','r+')
-lines = f.readlines() 
-f.seek(0) 
-f.write("token="+"'"+token+"'") 
-for line in lines:
-  f.write(line)
-f.close()
-print(R+"compiling script..."+W)
-time.sleep(7)
-print(B+"complete, your script /sdcard/CyberFox1/hackVK.py"+W)
-os.system("mkdir /sdcard/CyberFox1 && cp $HOME/CyberFox/core/hackVK.py /sdcard/CyberFox1")
-time.sleep(4)
+print(R+"closed to mess"+w)
+time.sleep(3)
